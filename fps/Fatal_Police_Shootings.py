d2b958cfca505f14fb6388bafbdcf6cbe64506c4
anatasiapham <116605966+anatasiapham@users.noreply.github.com>
2022-12-08 13:25:58 -0500

    Update Fatal_Police_Shootings.py

--- fps/Fatal_Police_Shootings.py
+++ fps/Fatal_Police_Shootings.py
@@ -6,7 +6,7 @@ import pandas as pd
 fps = pd.read_csv("fps/fatal-police-shootings-data.csv")
 
 st.header("US Fatal Police Shootings since 2015")
-st.image('police_drawing.jpeg', use_column_width=True)
+st.image('fps/police_drawing.jpeg', use_column_width=True)
 st.subheader('Purpose')
 st.markdown('Using public data collected by the Washington Post, I wanted to visualize and learn about the victims of fatal police shootings in the United States. The entire project can be found by visiting the following **[Github](https://github.com/anatasiapham/US_Fatal_Police_Shootings)**.')
 
