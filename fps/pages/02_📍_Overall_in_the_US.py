a6f18ddf29af4792d2ad8053bd87aca9b5bf7f5f
anatasiapham <116605966+anatasiapham@users.noreply.github.com>
2022-12-08 12:36:27 -0500

    Add files via upload

---
+++ fps/pages/02_📍_Overall_in_the_US.py
@@ -0,0 +1,154 @@
+import streamlit as st
+import altair as alt
+import pandas as pd
+from vega_datasets import data
+
+
+#Import the data set
+fps = pd.read_csv("fatal-police-shootings-data.csv")
+
+st.header('Overall in the United States')
+st.markdown('Adjust chart settings in the sidebar')
+
+#creating tabs
+tab1, tab2 = st.tabs(['US Map','Bar Chart'])
+
+#Dictionary for Race
+race_dict = {
+    "A":'Asian',
+    "B":'Black',
+    "H":'Hispanic',
+    "N":'Native American',
+    "O":'Other',
+    "W":'White'
+}
+
+#change races to be not abbreviated
+fps_race = fps.replace({"race":race_dict})
+
+#create dataframe that only includes geocoding accurate records
+geo = fps_race[fps_race['is_geocoding_exact']==True]
+geo_mod = geo[geo['name'] != "Gary Brown"].dropna() #dropped Gary Brown due to inaccurate coordinate, entry error?
+geo_mod = geo_mod[geo_mod['longitude'].notna()]
+
+#list of names
+names = geo_mod['name'].sort_values()
+
+st.sidebar.header('US Map Settings')
+selected = st.sidebar.selectbox('Search for specific person on the map:', names)
+st.sidebar.markdown('')
+st.sidebar.header('State Bar Chart Settings')
+min_count = st.sidebar.slider('Minimum Count of Deaths',10,400,100,10)
+label_button = st.sidebar.checkbox('Labels?')
+
+#create dataframe that includes only the person selected
+person_df = geo_mod[geo_mod['name'] == selected]
+
+#create dataframe that filters for states that meet the minimum count
+state_count = fps.groupby(['state'])['state'].count().reset_index(name='count')
+state_count_mod = state_count[state_count['count'] > min_count]
+
+
+with tab1:
+
+    st.write('')
+
+    states = alt.topo_feature(data.us_10m.url, feature='states')
+
+    background = alt.Chart(states).mark_geoshape(
+        fill='lightgray',
+        stroke='white'
+    ).project('albersUsa').properties(
+        width=700,
+        height=500
+    )   
+
+    points1 = alt.Chart(geo_mod, title='Victims of Fatal Police Encounters in the US').mark_circle(color='red').encode(
+        longitude='longitude:Q',
+        latitude='latitude:Q',
+        size=alt.value(10),
+        tooltip=['name', 'race', 'date']
+    ).project(
+        'albersUsa'
+    )
+
+    total_map = alt.layer(background, points1).configure_title(
+        fontSize=20
+        )
+    tab1.altair_chart(total_map, use_container_width=True)
+
+    st.write('')
+    st.markdown('These map charts visualize the locations of all the victims of fatal police shootings in the United States. Note that this map does not include the victims whose coordinates or names were not recorded (null).')
+    st.write('')
+    st.write('')
+
+
+    points2 = alt.Chart(geo_mod, title={"text": ['Victims of Fatal Police Encounters in the US'], "subtitle": ['Victim: 'f'{selected}']}).mark_circle(color='yellow').encode(
+        longitude='longitude:Q',
+        latitude='latitude:Q',
+        size=alt.value(10),
+        tooltip='name'
+    ).project(
+        'albersUsa'
+    ).properties(
+        width=700,
+        height=500
+    )   
+
+    highlight = alt.Chart(person_df).mark_circle(color='red').encode(
+        longitude='longitude:Q',
+        latitude='latitude:Q',
+        size=alt.value(30),
+        tooltip=['name', 'race', 'date']
+    ).project(
+        'albersUsa'
+    )
+
+    selected_map = alt.layer(background, points2, highlight).configure_title(
+        fontSize=20,
+        subtitleFontSize=16
+        )
+    tab1.altair_chart(selected_map, use_container_width=True)
+
+
+
+with tab2:
+    st.write('')
+
+    if label_button:
+        bar_chart = alt.Chart(state_count_mod, title="Total Fatal Police Shootings by State").mark_bar(color='red').encode(
+            x=alt.X('count:Q', title='Number of Fatal Police Shootings'),
+            y=alt.Y('state:N', title='States', sort = alt.EncodingSortField('count', op='sum', order='descending'))
+        )
+
+        text = bar_chart.mark_text(
+        align='left',
+        baseline='middle',
+        dx=3,
+        color='white'
+        ).encode(
+        text='count:Q'
+        )
+
+        bar = alt.layer(bar_chart, text).configure_axis(
+        labelFontSize=12,
+        titleFontSize=16
+        ).configure_title(
+        fontSize=24
+        )
+        tab2.altair_chart(bar, use_container_width=True)
+    else:
+        bar_chart = alt.Chart(state_count_mod, title="Total Fatal Police Shootings by State").mark_bar(color='red').encode(
+            x=alt.X('count:Q', title='Number of Fatal Police Shootings'),
+            y=alt.Y('state:N', title='States', sort = alt.EncodingSortField('count', op='sum', order='descending'))
+        ).configure_axis(
+            labelFontSize=11,
+            titleFontSize=13
+        ).configure_title(
+            fontSize=20
+        )
+
+        tab2.altair_chart(bar_chart, use_container_width=True)
+
+
+
---
+++ fps/pages/03_👤_Who_were_the_victims?.py
@@ -0,0 +1,126 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
+
+#Import the data set
+fps = pd.read_csv("fatal-police-shootings-data.csv")
+
+#Dictionary for Race
+race_dict = {
+    "A":'Asian',
+    "B":'Black',
+    "H":'Hispanic',
+    "N":'Native American',
+    "O":'Other',
+    "W":'White'
+}
+
+#Filter to drop null in race
+fps_race = fps[fps['race'].notna()]
+fps_race.info()
+
+fps_race = fps_race.replace({"race":race_dict})
+
+
+st.header('Who were the victims?')
+st.markdown("Representation of Deaths Reported")
+
+#creating tabs
+tab1, tab2, tab3 = st.tabs(['By Gender','By Age','By Race'])
+
+with tab1:
+    data = pd.DataFrame([dict(id=i) for i in range(1, 96)])
+
+    person = (
+        "M1.7 -1.7h-0.8c0.3 -0.2 0.6 -0.5 0.6 -0.9c0 -0.6 "
+        "-0.4 -1 -1 -1c-0.6 0 -1 0.4 -1 1c0 0.4 0.2 0.7 0.6 "
+        "0.9h-0.8c-0.4 0 -0.7 0.3 -0.7 0.6v1.9c0 0.3 0.3 0.6 "
+        "0.6 0.6h0.2c0 0 0 0.1 0 0.1v1.9c0 0.3 0.2 0.6 0.3 "
+        "0.6h1.3c0.2 0 0.3 -0.3 0.3 -0.6v-1.8c0 0 0 -0.1 0 "
+        "-0.1h0.2c0.3 0 0.6 -0.3 0.6 -0.6v-2c0.2 -0.3 -0.1 "
+        "-0.6 -0.4 -0.6z"
+    )
+
+    males = alt.Chart(data).transform_calculate(
+        row="ceil(datum.id/10)"
+    ).transform_calculate(
+        col="datum.id - datum.row*10"
+    ).mark_point(
+        filled=True,
+        size=50, color='#01A6EA'
+    ).encode(
+        x=alt.X("col:O", axis=None),
+        y=alt.Y("row:O", axis=None),
+        shape=alt.ShapeValue(person)
+    )
+
+    data2 = pd.DataFrame([dict(id=i) for i in range(1, 101)])
+
+    females = alt.Chart(data2).transform_calculate(
+        row="ceil(datum.id/10)"
+    ).transform_calculate(
+        col="datum.id - datum.row*10"
+    ).mark_point(
+        filled=True,
+        size=50, color='#FFB1CB'
+    ).encode(
+        x=alt.X("col:O", axis=None),
+        y=alt.Y("row:O", axis=None),
+        shape=alt.ShapeValue(person)
+    )
+
+    gender = alt.layer(females + males, title={"text":['Fatal Police Shootings by Gender'], "subtitle": [" "]}).configure_title(
+        fontSize=20
+    ).properties(
+        width=400,
+        height=400
+    ).configure_view(
+        strokeWidth=0
+    )
+
+    tab1.altair_chart(gender, use_container_width=True)
+
+
+    st.markdown("<h2 style='text-align: center;'>According to the genders reported, almost 95% of victims were male.</h1>", unsafe_allow_html=True)
+
+
+with tab2:
+
+    age_chart = alt.Chart(fps, title='Fatal Police Shootings by Age').mark_bar(color='red').encode(
+        y=alt.Y('count():Q', title='Number of Fatal Police Shootings'),
+        x=alt.X('age', title='Age Range', bin=True)
+    ).configure_axis(
+        labelFontSize=11,
+        titleFontSize=16
+    ).configure_title(
+        fontSize=20
+    ).properties(
+        width=700,
+        height=500
+    )   
+    tab2.altair_chart(age_chart, use_container_width=True)
+
+    st.markdown("<h2 style='text-align: center;'>Most of the victims ages ranged from 20 to 50.</h1>", unsafe_allow_html=True)
+
+
+with tab3:
+
+    race_chart = alt.Chart(fps_race, title="Fatal Police Shootings by Race").mark_bar().encode(
+        y=alt.Y('count():Q', title='Number of Fatal Police Shootings'),
+        x=alt.X('race', title='Race', axis=alt.Axis(labelAngle=0), sort='-y'),
+        color=alt.Color('race', legend=alt.Legend(title='Races'))
+    ).configure_axis(
+        labelFontSize=11,
+        titleFontSize=16,
+        grid=False
+    ).configure_title(
+        fontSize=20
+    ).properties(
+        width=700,
+        height=500
+    )
+
+    tab3.altair_chart(race_chart, use_container_width=True)
+
+    st.markdown("<h2 style='text-align: center;'>Of the races reported, a vast majority of the victims were White, Black, or Hispanic.</h1>", unsafe_allow_html=True)
+   
